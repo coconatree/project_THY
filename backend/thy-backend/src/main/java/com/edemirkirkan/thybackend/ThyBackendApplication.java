336b3d5dabb720eee9295c700bd752894d170351
edemirkirkan <ege.demirkirkan@ug.bilkent.edu.tr>
2022-07-31 14:48:09 +0300

    Add general structure

---
+++ backend/thy-backend/src/main/java/com/edemirkirkan/thybackend/ThyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edemirkirkan.thybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/thy-backend/src/test/java/com/edemirkirkan/thybackend/ThyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edemirkirkan.thybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
