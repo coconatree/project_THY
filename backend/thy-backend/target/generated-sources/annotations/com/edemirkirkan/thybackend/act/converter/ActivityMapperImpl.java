d4cd16adf7fcf850a6bf3c7b951106ee938c53a8
coconatree <emre.caniklioglu.00@gmail.com>
2022-08-04 14:14:34 +0300

    Frontend page transition - Global State Improvement - Customer Service Improvement

--- backend/thy-backend/target/generated-sources/annotations/com/edemirkirkan/thybackend/act/converter/ActivityMapperImpl.java
+++ backend/thy-backend/target/generated-sources/annotations/com/edemirkirkan/thybackend/act/converter/ActivityMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-08-04T10:42:35+0300",
+    date = "2022-08-04T13:22:06+0300",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 18.0.2 (Oracle Corporation)"
 )
 @Component
