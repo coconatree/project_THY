a1fe2110574ef35abf1501b18c36d3c4decf4dc8
berksaltuk <saltuk.yilmaz@ug.bilkent.edu.tr>
2022-08-05 19:40:59 +0300

    Forgot to commit that one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,25 +9,25 @@ import "./static/style/main.css";
 import "./index.css";
 
 function App() {
-  return (
-    <div className="bacground_image bg-black h-full w-full">
-      <div className="h-full w-full">
-        <Navbar /*className = "brightness-110 bg-slate-900 bg-opacity-30 w-full"*/
-        />
-        <div>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <ReservationCodePage className="flex flex-col bg-slate-900 bg-opacity-60 justify-center brightness-110 h-full w-full" />
-              }
-            />
-            <Route path="/activities" element={<ActivitiesPage />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='bacground_image bg-black h-full w-full'>
+			<div className='h-full w-full opacityBackground'>
+				<Navbar /*className = "brightness-110 bg-slate-900 bg-opacity-30 w-full"*/
+				/>
+				<div>
+					<Routes>
+						<Route
+							path='/'
+							element={
+								<ReservationCodePage className='flex flex-col bg-slate-900 bg-opacity-60 justify-center brightness-110 h-full w-full' />
+							}
+						/>
+						<Route path='/activities' element={<ActivitiesPage />} />
+					</Routes>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
